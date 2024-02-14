703339651aad63d340d50280eaaca71b83055894
Bl4ckNblue <137901620+Bl4ckNblue@users.noreply.github.com>
2024-02-14 08:08:19 -0500

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -12,23 +12,28 @@ if inicio == "s":
                 if areaelegida == 1:
                     ci = area_circulo()
                     print (f"El área del circulo es {ci:.2f}")
+                    break
                 elif areaelegida == 2:
                     cua = area_cuadrado()
                     print (f"El área del cuadrado es {cua:.2f}")
+                    break
                 elif areaelegida == 3:
                     rec = area_rectangulo()
                     print (f"El área del rectangulo es {rec:.2f}")
+                    break
                 elif areaelegida == 4:
                     tri = area_triangulo()
                     print (f"El área del triángulo es {tri:.2f}")
+                    break
                 elif areaelegida == 5:
                     tra = area_trapecio()
                     print (f"El área del trapecio es {tra:.2f}")
+                    break
                 else:
                     print ("\nIngrese una opción valida")
                     print ("\n1.-Circulo \n2.-Cuadrado \n3.-Rectángulo \n4.-Triángulo \n5.-Trapecio")
                     areaelegida = int(input("\nIngrese el número de la figura de la cual hallará el área: "))
-                break
+                
             break
         except ValueError:
             print ("ERROR: Ingrese una opción con caracteres numericos")
